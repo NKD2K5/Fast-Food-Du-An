b937fc6217ac22244d1811434f9bcff7d5398b61
NKD2K5 <duytoan20052011@gmail.com>
2025-01-17 22:44:56 +0700

    Reapply "Duy update"
    This reverts commit 5e1ff4210aed5a915d2e32ba8db15c16ba171c2a.

--- Fast-Food/Program.cs
+++ Fast-Food/Program.cs
@@ -1,3 +1,6 @@
+﻿using Fast_Food.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Fast_Food
 {
     public class Program
@@ -6,29 +9,36 @@ namespace Fast_Food
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Đăng ký DbContext với chuỗi kết nối
+            builder.Services.AddDbContext<DoAnStoreContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            });
+
+            // Đăng ký Session
+            builder.Services.AddSession(options => options.IOTimeout = TimeSpan.FromSeconds(5));
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Cấu hình pipeline
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseRouting();
 
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=DangNhap}/{action=Login}/{id?}");
 
             app.Run();
         }
